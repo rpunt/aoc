3abdd9df99a74616ed5015617fdb396febd77e78
Ryan Punt <ryan.punt@bestbuy.com>
2022-12-03 12:22:21 -0600

    day1 go

---
+++ 2022/01/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+
+	"github.com/rpunt/aoc/utils"
+)
+
+func main() {
+	input := utils.ReadFile("inputs.txt")
+
+	fmt.Printf("part 1: %v\n", Part1(input))
+	fmt.Printf("part 2: %v\n", Part2(input))
+}
+
+func Part1(calories []string) int {
+	calories_by_elf := total_elvish_calories(calories)
+	return topX(calories_by_elf, 1)
+}
+
+func Part2(calories []string) int {
+	calories_by_elf := total_elvish_calories(calories)
+	return topX(calories_by_elf, 3)
+}
+
+func topX(calories []int, x int) int {
+	// sum the x highest array values
+	sort.Sort(sort.Reverse(sort.IntSlice(calories)))
+	summed_calories := 0
+	for i := 0; i < x; i++ {
+		summed_calories += calories[i]
+	}
+
+	return summed_calories
+}
+
+func total_elvish_calories(inventory []string) []int {
+	// return a new array containing per-elf caloric totals
+	var elvish_calorie_load []int
+	calories := 0
+	for _, food := range inventory {
+		calorie, err := strconv.Atoi(food)
+		if err != nil {
+			elvish_calorie_load = append(elvish_calorie_load, calories)
+			calories = 0
+		}
+		calories += calorie
+	}
+	elvish_calorie_load = append(elvish_calorie_load, calories)
+	return elvish_calorie_load
+}
+
+func sliceAtoi(sa []string) []int {
+	si := make([]int, 0, len(sa))
+	for _, a := range sa {
+		i, err := strconv.Atoi(a)
+		utils.CheckErr(err)
+		si = append(si, i)
+	}
+
+	return si
+}
+
+/*
+part 1: 70116
+part 2: 206582
+*/
