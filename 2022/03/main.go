48bedec780091dfc4ebd5a2c949494d55533746c
Ryan Punt <ryan.punt@bestbuy.com>
2022-12-03 22:45:19 -0600

    adding more

---
+++ 2022/03/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rpunt/aoc/utils"
+)
+
+func main() {
+	input := utils.ReadFile("input.txt")
+
+	fmt.Printf("part 1: %v\n", Part1(input))
+	fmt.Printf("part 2: %v\n", Part2(input))
+}
+
+func Part1(calories []string) int {
+	return 0
+}
+
+func Part2(calories []string) int {
+	return 0
+}
