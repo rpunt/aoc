5c5ee50f5bc21c615406d27a480fb327a7bcc1f2
Ryan Punt <ryan.punt@bestbuy.com>
2022-12-04 08:16:02 -0600

    add pulldata

---
+++ pulldata.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'nokogiri'
+require 'time'
+require 'optimist'
+require 'httparty'
+require 'fileutils'
+
+today = Time.new
+
+options = Optimist.options do
+  opt :year, 'data: target year', type: :integer, default: today.year
+  opt :day, 'data: target day', type: :integer, default: today.day
+end
+
+abort('2020 is the earliest year supported at this time') if options[:year] < 2020
+
+# A function to download test data and problemsets for adventofcode
+#
+# @param year [Integer] the year in which the problem was presented
+# @param day [Integer] the day of the year in which the problem was presented
+#
+# @return [String]
+def getdata(year, day)
+  # return HTTP response body for the chosen year and day
+  response = HTTParty.get("https://adventofcode.com/#{year}/day/#{day}")
+  case response.code
+  when 200
+    response.body
+  when 404
+    abort('Page not found')
+  end
+end
+
+# A function to write data to a path, creating required paths along the way
+#
+# @param path [String] the path to which the data should be written, including filename
+# @param data [String] the data to be written
+#
+# @return [void]
+def writedata(path, data)
+  FileUtils.mkdir_p(File.dirname(path))
+  File.write(path, data)
+end
+
+# set the xpath selectors appropriate for the year in question
+xpath_selector = {
+  2022 => {
+    'parent_readme' => '//article',
+    'testdata' => '//pre/code'
+  },
+  2021 => {
+    'parent_readme' => '//article',
+    'testdata' => '//pre/code'
+  },
+  2020 => {
+    'parent_readme' => '//article',
+    'testdata' => '//pre/code'
+  }
+}
+
+data = Nokogiri::HTML.parse(getdata(options[:year], options[:day]))
+
+parent_readme_data = data.xpath(xpath_selector[options[:year]]['parent_readme']).text
+                         .gsub(/\n/, "\n\n")
+                         .gsub(/^---/, '#')
+                         .gsub(/ ---/, "\n\n## Part 1\n\n")
+testable_inputs_data = data.xpath(xpath_selector[options[:year]]['testdata']).text
+
+writedata("#{__dir__}/#{options[:year]}/#{'%02d' % options[:day]}/README.md", parent_readme_data)
+writedata("#{__dir__}/#{options[:year]}/#{'%02d' % options[:day]}/inputs/README.md", "# testable inputs\n\nToday's example data is in `testable.txt`; you can add your own dataset as well.")
+writedata("#{__dir__}/#{options[:year]}/#{'%02d' % options[:day]}/inputs/testable.txt", testable_inputs_data)
+
+puts "Please review the contents of README.md and #{options[:year]}/#{'%02d' % options[:day]}/README.md for formatting errors."
