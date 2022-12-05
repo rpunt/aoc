32bcf52dbff875614306fb78a16f8e9c19076ac6
Ryan Punt <ryan@mirum.org>
2022-12-04 21:49:07 -0600

    add day2

--- 2022/02/main.go
+++ 2022/02/main.go
@@ -2,16 +2,72 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	utils "github.com/rpunt/aoc_utils"
 )
 
 type Choice int
 
+const (
+	Unknown Choice = iota
+	Rock
+	Paper
+	Scissors
+)
+
+func SymbolToValue(symbol string) Choice {
+	switch symbol {
+	case "A", "X":
+		return Rock
+	case "B", "Y":
+		return Paper
+	case "C", "Z":
+		return Scissors
+	default:
+		return Unknown
+	}
+}
+
+func NewRequiredChoice(c Choice, s string) Choice {
+	switch s {
+	case "X": // must lose
+		switch c {
+		case Rock:
+			return Scissors
+		case Paper:
+			return Rock
+		case Scissors:
+				return Paper
+		}
+
+	case "Y": // must draw
+		return c
+
+	case "Z": // must win
+		switch c {
+		case Rock:
+			return Paper
+		case Paper:
+			return Scissors
+		case Scissors:
+			return Rock
+		}
+	}
+
+	return Unknown
+}
 
 type Round struct {
 	Player1 Choice
 	Player2 Choice
+}
+
+func NewRound(ch1, ch2 Choice) *Round {
+	return &Round{
+		Player1: ch1,
+		Player2: ch2,
+	}
 }
 
 func (c Choice) Score() int {
@@ -23,7 +79,7 @@ func (r Round) TotalScore() int {
 
 	// draw
 	if r.Player1 == r.Player2 {
-		return score + 3
+		return roundScore + 3
 	}
 
 	if r.Player1 == Rock {
@@ -54,16 +110,37 @@ func main() {
 	fmt.Printf("part 2: %v\n", Part2(input))
 }
 
-const {
-	int Rock = 1
-	int Paper = 2
-	int Scissors = 3
-}
-
 func Part1(calories []string) int {
-	return 0
+	rounds := []*Round{}
+
+	for _, line := range calories {
+		arr := strings.Split(line, " ")
+		round := NewRound(SymbolToValue(arr[0]), SymbolToValue(arr[1]))
+		rounds = append(rounds, round)
+	}
+
+	totalScore := 0
+	for _, r := range rounds {
+		totalScore += r.TotalScore()
+	}
+
+	return totalScore
 }
 
 func Part2(calories []string) int {
-	return 0
+	rounds := []*Round{}
+
+	for _, line := range calories {
+		arr := strings.Split(line, " ")
+		ch1 := SymbolToValue(arr[0])
+		round := NewRound(ch1, NewRequiredChoice(ch1, arr[1]))
+		rounds = append(rounds, round)
+	}
+
+	totalScore := 0
+	for _, r := range rounds {
+		totalScore += r.TotalScore()
+	}
+
+	return totalScore
 }
