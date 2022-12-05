449e2c7764c62ef869b7a6705e19c43327146311
Ryan Punt <ryan@mirum.org>
2022-12-05 00:46:05 -0600

    start day5

---
+++ 2022/05/main.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+require 'optimist'
+
+options = Optimist::options do
+  opt :inputfile, "which inputfile should I use?", :type => :string, :default => 'input'
+  opt :testing, "use testing inputs?", :type => :bool, :default => false
+end
+
+$debug = false
+if options[:testing]
+  options[:inputfile] = 'testable'
+  $debug = true
+end
+
+inputs = []
+File.open("#{__dir__}/#{options[:inputfile]}.txt").each { |line| inputs << line.chomp }
+
+#     [D]
+# [N] [C]
+# [Z] [M] [P]
+#  1   2   3
+
+finished_stack_description = false
+started_directions = false
+boxes = []
+directions = []
+inputs.each do |line|
+  if line.empty?
+    finished_stack_description = true
+    started_directions = true
+    next
+  end
+
+  column_count = 0
+  unless finished_stack_description
+    inputs.each do |line|
+      finished_stack_description = true if line.empty?
+      column_count = line.length if line.length > column_count
+      boxes << line
+    end
+  end
+  if started_directions
+    directions << line
+  end
+end
+require 'pry'; binding.pry
