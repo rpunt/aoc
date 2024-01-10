815542285ee2854ceaeed669d020c445e0a06e46
Ryan Punt <ryan.punt@bestbuy.com>
2024-01-09 20:25:01 -0600

    update python template

--- template/main.py
+++ template/main.py
@@ -22,15 +22,17 @@ def Part2(input):
   return 0
 
 part1 = Part1(input)
-print(part1)
 if args.testing:
   test_value_part_1 = 999
   if part1 != test_value_part_1:
     print('TEST VALUE IS WRONG: got {}, wanted {}'.format(part1, test_value_part_1))
+  else:
+    print(part1)
 
 part2 = Part2(input)
-print(part2)
 if args.testing:
   test_value_part_2 = 999
   if part1 != test_value_part_2:
     print('TEST VALUE IS WRONG: got {}, wanted {}'.format(part2, test_value_part_2))
+  else:
+    print(part2)
