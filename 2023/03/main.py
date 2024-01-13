2ad5fe3900733ea42f7a9fc1d7408678aef1e563
Ryan Punt <ryan@mirum.org>
2024-01-13 13:31:48 -0600

    prep 2023-03

---
+++ 2023/03/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import argparse
+from colorama import Fore
+
+parser = argparse.ArgumentParser(description="specify input file")
+parser.add_argument('-i', '--inputfile', metavar="Inputfile", type=str, help="which input file should I use?", default='input')
+parser.add_argument('-t', '--testing', help = 'use testing inputs', action = 'store_true')
+
+args = parser.parse_args()
+
+if args.testing == True:
+  args.inputfile = 'testable'
+
+path = args.inputfile + ".txt"
+
+input = [line.rstrip() for line in open(path, 'r').readlines()]
+
+def Part1(input):
+  return 0
+
+def Part2(input):
+  return 0
+
+part1 = Part1(input)
+if args.testing:
+  test_value_part_1 = 4361
+  if part1 != test_value_part_1:
+    print(Fore.RED + 'TEST VALUE IS WRONG: got {}, wanted {}'.format(part1, test_value_part_1))
+  else:
+    print(Fore.GREEN + 'Test 1: {}'.format(part1))
+else:
+  print(part1)
+
+part2 = Part2(input)
+if args.testing:
+  test_value_part_2 = 999
+  if part1 != test_value_part_2:
+    print(Fore.RED + 'TEST VALUE IS WRONG: got {}, wanted {}'.format(part2, test_value_part_2))
+  else:
+    print(Fore.GREEN + 'Test 2: {}'.format(part2))
+else:
+  print(part2)
---
+++ 2023/03/main.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require 'optimist'
+
+options = Optimist::options do
+  opt :inputfile, "which inputfile should I use?", :type => :string, :default => 'input'
+  opt :testing, "use testing inputs?", :type => :bool, :default => false
+end
+
+$debug = false
+if options[:testing]
+  options[:inputfile] = 'testable'
+  $debug = true
+end
+
+inputs = File.read("#{__dir__}/#{options[:inputfile]}.txt").split
+
+def Part1(inputs)
+  return 0
+end
+
+def Part2(inputs)
+  return 0
+end
+
+part1 = Part1(inputs)
+part2 = Part2(inputs)
+
+puts "Part 1: #{part1}"
+if $debug
+  test_value_part_1 = 999
+  if part1 != test_value_part_1
+    puts "TEST VALUE IS WRONG: got #{part1}, wanted #{test_value_part_1}"
+  end
+end
+
+puts "Part 2: #{part2}"
+if $debug
+  test_value_part_2 = 999
+  if part2 != test_value_part_2
+    puts "TEST VALUE IS WRONG: got #{part2}, wanted #{test_value_part_2}"
+  end
+end
