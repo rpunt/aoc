817a7248f1f868580f279957feb9f4b13c70744b
Ryan Punt <ryan@mirum.org>
2024-01-13 13:34:17 -0600

    update default python template

--- template/main.py
+++ template/main.py
@@ -3,6 +3,7 @@
 import argparse
 from colorama import Fore
 
+#region setup
 parser = argparse.ArgumentParser(description="specify input file")
 parser.add_argument('-i', '--inputfile', metavar="Inputfile", type=str, help="which input file should I use?", default='input')
 parser.add_argument('-t', '--testing', help = 'use testing inputs', action = 'store_true')
@@ -15,6 +16,7 @@ if args.testing == True:
 path = args.inputfile + ".txt"
 
 input = [line.rstrip() for line in open(path, 'r').readlines()]
+#endregion
 
 def Part1(input):
   return 0
@@ -22,15 +24,16 @@ def Part1(input):
 def Part2(input):
   return 0
 
+#region answers
 part1 = Part1(input)
 if args.testing:
-  test_value_part_1 = 8
+  test_value_part_1 = 999
   if part1 != test_value_part_1:
     print(Fore.RED + 'TEST VALUE IS WRONG: got {}, wanted {}'.format(part1, test_value_part_1))
   else:
     print(Fore.GREEN + 'Test 1: {}'.format(part1))
 else:
-  print(part1)
+  print(Fore.CYAN + '{}'.format(part1))
 
 part2 = Part2(input)
 if args.testing:
@@ -40,4 +43,5 @@ if args.testing:
   else:
     print(Fore.GREEN + 'Test 2: {}'.format(part2))
 else:
-  print(part2)
+  print(Fore.CYAN + '{}'.format(part2))
+#endregion
